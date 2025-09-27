52b884f3295559d618013e1389a89231f85d8d6b
cenereh <s7H28NsjAOs@protonmail.com>
2025-09-27 12:30:51 +0200

    go debugger

--- init.lua
+++ init.lua
@@ -889,6 +889,39 @@ require('lazy').setup({
     },
   },
 
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'leoluz/nvim-dap-go',
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
+    config = function()
+      require('dapui').setup()
+      require('dap-go').setup()
+
+      local dap, dapui = require 'dap', require 'dapui'
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set('n', '<Leader>dt', ':DapToggleBreakpoint<CR>')
+      vim.keymap.set('n', '<Leader>dc', ':DapContinue<CR>')
+      vim.keymap.set('n', '<Leader>dx', ':DapTerminate<CR>')
+      vim.keymap.set('n', '<Leader>do', ':DapStepOver<CR>')
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
